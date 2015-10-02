b03458e3ec8a366c10b889589147c7e633f7f185
Prasun Thapa <thapap@student.swosu.edu>
2015-10-02 10:43:26 -0500

    Initial start

---
+++ src/Thapa.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAB
+ *
+ */
+public class Thapa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");
+	}
+	
+
+}
